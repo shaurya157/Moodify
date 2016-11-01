48be9ff66756de3eced0aa5bfe06903737c7a9ba
Shaurya Sinha <shaurya157@gmail.com>
2016-10-31 21:39:26 -0700

    user model and database set up

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+class User < ActiveRecord::Base
+  validates :username, :password_digest, presence: true
+  validates :username, uniqueness: true
+
+  after_intialize :ensure_session_token
+  attr_reader :password
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_token!
+    self.session_token = SecureRandom.urlsafe_base64
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+end
