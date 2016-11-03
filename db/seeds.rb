46f9323ac060403da24c91ced8ebf6085a270a04
apple <apple@apples-MacBook-Pro.local>
2016-11-03 11:58:38 -0700

    basic database for songs setup along with songs controller with an index view and action. Need to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create({username: 'DemoUser', password: 'password'})
+
+Song.create({title: 'Viva la Vida', artist: 'Coldplay',
+            album: 'Death and all his friends', song_image_url: 'http://res.cloudinary.com/djv7nouxz/image/upload/v1478198878/art_vivalavida_ed7e9i.jpg',
+            audio_url:'http://res.cloudinary.com/djv7nouxz/video/upload/v1478198769/Coldplay_-_Viva_La_Vida_eozpbm.mp3',
+            song_duration: '4:02'})
+
+Song.create({title: 'Hymn for the Weekend', artist: 'Coldplay',
+            album: 'Hymn for the Weekend', song_image_url: 'http://res.cloudinary.com/djv7nouxz/image/upload/v1478198878/hqdefault_qfszvd.jpg',
+            audio_url:'http://res.cloudinary.com/djv7nouxz/video/upload/v1478198887/Coldplay_-_Hymn_For_The_Weekend_Official_video_j3rghk.mp3',
+            song_duration: '4:20'})
+
+Song.create({title: 'Strawberry Swing', artist: 'Coldplay',
+            album: 'Death and all his friends', song_image_url: 'http://res.cloudinary.com/djv7nouxz/image/upload/v1478198878/220px-Coldplaystrawberryswing_j9emrx.jpg',
+            audio_url:'http://res.cloudinary.com/djv7nouxz/video/upload/v1478198888/Coldplay_-_Strawberry_Swing_gmar7g.mp3',
+            song_duration: '4:13'})
