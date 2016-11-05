1ed9e1331ee7e8b94ce7f7e8e8cad8814669e4b7
Shaurya Sinha <shaurya157@gmail.com>
2016-11-04 22:22:32 -0700

    working on playbar

--- frontend/components/player/song_player.jsx
+++ frontend/components/player/song_player.jsx
@@ -4,9 +4,11 @@ import ReactPlayer from 'react-player';
 class SongPlayer extends React.Component {
   constructor(props){
     super(props);
+    this.progress = 0;
     this.togglePlay = this.togglePlay.bind(this);
     this.startNextSong = this.startNextSong.bind(this);
     this.startPreviousSong = this.startPreviousSong.bind(this);
+    this.updatePlaybar = this.updatePlaybar.bind(this);
   }
 
   togglePlay (){
@@ -18,11 +20,19 @@ class SongPlayer extends React.Component {
   }
 
   startNextSong(){
-    this.props.goForward();
+    if(this.props.playqueue.length === 1){
+      this.props.pauseSong();
+    } else {
+      this.props.goForward();
+    }
   }
 
   startPreviousSong(){
     this.props.goBack();
+  }
+
+  updatePlaybar({played}){
+    this.progress = played * 100;
   }
 
   render () {
@@ -32,10 +42,22 @@ class SongPlayer extends React.Component {
         url={this.props.song.audio_url}
         playing={this.props.playing} hidden={true}
         className='song-player'
-        onEnded={this.startNextSong}/>
+        onEnded={this.startNextSong}
+        onProgress={this.updatePlaybar}/>
+
+        <div className='progress-bar'>
+          <div className='audio-progress'
+            style={{width: `${document.documentElement.clientWidth*this.progress}%`}}>
+          </div>
+          <div className='progress-circle'
+            style={{left: `${document.documentElement.clientWidth*this.progress - 8}px`}}>
+          </div>
+        </div>
+
         <button onClick={this.togglePlay}>Toggle play</button>
         <button onClick={this.startPreviousSong}>Previous</button>
         <button onClick={this.startNextSong}>Next</button>
+
       </div>
   );
   }
