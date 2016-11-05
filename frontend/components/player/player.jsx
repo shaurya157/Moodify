6ecd50023e96d44ad7cf8de08da218a1043e7e9c
Shaurya Sinha <shaurya157@gmail.com>
2016-11-05 11:12:05 -0700

    styling for player

--- frontend/components/player/player.jsx
+++ frontend/components/player/player.jsx
@@ -10,12 +10,20 @@ class Player extends React.Component {
     let song = this.props.song;
 
     if(song.id){
+      let divStyle = {
+        backgroundImage: `url(${song.song_image_url})`,
+        backgroundSize: `cover`,
+        backgroundPosition: `50%`,
+      };
+
       return(
         <div className='player'>
           <div className='current-song-details'>
-            <img src={song.song_image_url}></img>
-            <div>{song.title}</div>
-            <div>{song.artist}</div>
+            <div className='current-song-image'
+              style={divStyle}>
+            </div>
+            <div className='current-song-title'>{song.title}</div>
+            <div className='current-song-artist'>{song.artist}</div>
           </div>
           <SongPlayerContainer />
         </div>
