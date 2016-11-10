bb0e32d720a0cfbf5b936d265617f7c1b07868d7
Shaurya Sinha <shaurya157@gmail.com>
2016-11-09 22:08:12 -0800

    adding songs to playlist working

--- app/views/api/users/show_user.json.jbuilder
+++ app/views/api/users/show_user.json.jbuilder
@@ -1,1 +1,3 @@
-json.extract! @user, :username, :id, :bio, :profile_image_url, :followed_playlists, :followers
+json.extract! @user, :username, :id, :bio, :profile_image_url, :followers
+json.followedPlaylists @user.followed_playlists
+json.createdPlaylists @user.playlists
--- frontend/components/playlist_modal/song_actions_container.js
+++ frontend/components/playlist_modal/song_actions_container.js
@@ -4,12 +4,14 @@ import {addSongToPlaylist} from '../../actions/playlist_actions';
 import {addSongToQueue} from '../../actions/audio_actions';
 
 const mapStateToProps = state => ({
-  followedPlaylists: state.playlists.followedPlaylists
+  followedPlaylists: state.playlists.followedPlaylists,
+  createdPlaylists: state.playlists.createdPlaylists
 });
 
 const mapDispatchToProps = dispatch => ({
   addSongToPlaylist: (songId, playlistId) => dispatch(addSongToPlaylist(songId, playlistId)),
-  addSongToQueue: (song) => dispatch(addSongToQueue(song))
+  addSongToQueue: (song) => dispatch(addSongToQueue(song)),
+  addSongToPlaylist: (songId, playlistId) => dispatch(addSongToPlaylist(songId, playlistId))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SongActions);
